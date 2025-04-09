49c3fe3424432630b13f6dc61ebf3f6e3f43bee4
Ömer Apaydın <omerapaydin90@gmail.com>
2025-04-10 00:47:05 +0300

    Initial Commit

---
+++ SearchBarTableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchBarTableView
+//
+//  Created by Ömer on 10.04.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SearchBarTableViewTests/SearchBarTableViewTests.swift
@@ -0,0 +1,17 @@
+//
+//  SearchBarTableViewTests.swift
+//  SearchBarTableViewTests
+//
+//  Created by Ömer on 10.04.2025.
+//
+
+import Testing
+@testable import SearchBarTableView
+
+struct SearchBarTableViewTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
